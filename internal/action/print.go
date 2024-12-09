1712e4785a3257609eac95cd2c742a30c11ac7e5
Fang Yuan <wojiushifangyuanlove@gmail.com>
2024-12-09 17:09:30 +0800

    feat: 更新打印规则
    Signed-off-by: Fang Yuan <wojiushifangyuanlove@gmail.com>

--- internal/action/print.go
+++ internal/action/print.go
@@ -2,9 +2,12 @@ package action
 
 import (
 	"fmt"
+	"os"
 
+	// "github.com/767829413/easy-novel/internal/action/source"
 	"github.com/767829413/easy-novel/internal/config"
 	"github.com/fatih/color"
+	"github.com/olekukonko/tablewriter"
 	"github.com/sirupsen/logrus"
 )
 
@@ -32,10 +35,31 @@ func NewPrintHint(l *logrus.Logger, version string) Action {
 
 func (p *printHint) Execute() error {
 	cfg := config.GetConf()
-	color.Blue("easy-novel %s （本项目开源免费）", p.version)
-	fmt.Println("开源地址：https://github.com/767829413/easy-novel")
-	fmt.Printf("当前书源：%d\n", cfg.Base.SourceID)
-	color.Cyan("导出格式：%s", cfg.Base.Extname)
-	color.Yellow("请务必阅读 readme.txt")
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"使用须知"})
+	table.SetBorder(true)
+	table.SetColumnSeparator("")
+	table.SetRowSeparator("")
+	table.SetCenterSeparator("")
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	table.SetHeaderLine(false)
+	table.SetBorders(tablewriter.Border{Left: true, Top: true, Right: true, Bottom: true})
+	table.SetAutoWrapText(false)
+
+	// 使用color.New()创建彩色文本
+	blue := color.New(color.FgBlue).SprintFunc()
+	cyan := color.New(color.FgCyan).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintFunc()
+
+	table.Append([]string{blue(fmt.Sprintf("easy-novel %s （本项目开源且免费）", p.version))})
+	table.Append([]string{fmt.Sprintf("官方地址：%s", "https://github.com/767829413/easy-novel")})
+	table.Append([]string{cyan(fmt.Sprintf("当前书源：%s (ID: %d)", "未实现", cfg.Base.SourceID))})
+	table.Append([]string{cyan(fmt.Sprintf("导出格式：%s", cfg.Base.Extname))})
+	table.Append([]string{""}) // 空行
+	table.Append([]string{yellow("请务必阅读 readme.txt")})
+
+	table.Render()
+
 	return nil
 }
